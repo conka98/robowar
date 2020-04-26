8bf725bd46982c7af702e4ce62a2da8a7b8c1993
conk20 <arunsinghsuthar@gmail.com>
2020-04-26 22:50:36 +0530

    Updates

---
+++ precache-manifest.76f50c3c12c4f5a18cd9da8661cdb6da.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "727ca08a12fced597d283a0ec8a0ef84",
+    "url": "/robowar/index.html"
+  },
+  {
+    "revision": "01dda8862d92b797dfaf",
+    "url": "/robowar/static/css/2.66b7f74f.chunk.css"
+  },
+  {
+    "revision": "1a15160fed768709bf26",
+    "url": "/robowar/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "01dda8862d92b797dfaf",
+    "url": "/robowar/static/js/2.a10038f9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robowar/static/js/2.a10038f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a15160fed768709bf26",
+    "url": "/robowar/static/js/main.2a1b147f.chunk.js"
+  },
+  {
+    "revision": "84e0f8a73ebf00f9c98f",
+    "url": "/robowar/static/js/runtime-main.437d9183.js"
+  }
+]);
